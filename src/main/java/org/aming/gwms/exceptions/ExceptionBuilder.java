62296f8464537aef89db16ba4694c1f88c05da3d
Arthur Ming <937765518@qq.com>
2017-09-02 23:35:39 +0800

    rename 'AmingExceptionBuilder' --> 'ExceptionBuilder'

---
+++ src/main/java/org/aming/gwms/exceptions/ExceptionBuilder.java
@@ -0,0 +1,22 @@
+package org.aming.gwms.exceptions;
+
+import org.aming.gwms.logger.AmingLogger;
+import org.aming.gwms.logger.LoggerManager;
+
+/**
+ * 自定义长城内部管理系统异常管理器
+ * @author daming
+ * @version 2017/6/22.
+ */
+public class ExceptionBuilder {
+
+    /**
+     * 内置的日志记录器
+     */
+    private static AmingLogger logger = LoggerManager.getLogger(ExceptionBuilder.class.getName());
+
+    public static AmingException buildAmingEzception(String errorCode,Throwable cause){
+        return null;
+    }
+
+}
