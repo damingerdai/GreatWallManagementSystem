30ec754b8d24a7921a71eb764395d960c6a3a948
Arthur Ming <937765518@qq.com>
2017-09-02 21:31:02 +0800

    add utils for java

--- src/main/java/org/aming/gwms/utils/PropertyLoader.java
+++ src/main/java/org/aming/gwms/utils/PropertyLoader.java
@@ -1,8 +1,20 @@
 package org.aming.gwms.utils;
 
 /**
+ * 配置文件加载器
  * @author daming
  * @version 2017/7/4.
  */
 public class PropertyLoader {
+    private PropertyLoader(){}
+
+    /**
+     * 加载配置文件
+     * @param propertyFiles
+     */
+    public static void loadProperty(String... propertyFiles){
+        for(String file : propertyFiles){
+
+        }
+    }
 }
--- src/main/java/org/aming/gwms/utils/StringUtils.java
+++ src/main/java/org/aming/gwms/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package org.aming.gwms.utils;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 /**
  * 自定义字符串工具类
  * @author daming
@@ -7,4 +9,12 @@ package org.aming.gwms.utils;
  */
 public class StringUtils extends org.apache.commons.lang.StringUtils {
 
+    /**
+     * 将对象转为String类型
+     * @param obj
+     * @return
+     */
+    public static String reflectionToString (Object obj){
+        return ToStringBuilder.reflectionToString(obj);
+    }
 }
