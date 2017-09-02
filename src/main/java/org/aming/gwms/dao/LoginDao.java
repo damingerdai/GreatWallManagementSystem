15eb416d381c6fcfa2415d4ce068b9158563d0c7
Arthur Ming <937765518@qq.com>
2017-09-02 21:30:07 +0800

    update login servlet

--- src/main/java/org/aming/gwms/dao/LoginDao.java
+++ src/main/java/org/aming/gwms/dao/LoginDao.java
@@ -1,8 +1,17 @@
 package org.aming.gwms.dao;
 
 /**
+ * 登录模块的DAO
  * @author daming
  * @version 2017/7/4.
  */
 public interface LoginDao {
+
+    /**
+     * 验证用户是否存在
+     * @param name
+     * @param password
+     * @return
+     */
+    int verify(String name,String password);
 }
--- src/main/java/org/aming/gwms/servlets/LoginServlet.java
+++ src/main/java/org/aming/gwms/servlets/LoginServlet.java
@@ -1,8 +1,33 @@
 package org.aming.gwms.servlets;
 
+import org.aming.gwms.service.LoginService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author daming
  * @version 2017/6/27.
+ * 登陆Servlet
  */
-public class LoginServlet {
+public class LoginServlet extends HttpServlet {
+
+    private LoginService loginService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    private void verify(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
