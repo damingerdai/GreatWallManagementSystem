80343b9a990c7d618f75f8038aaf4773d3ce1a91
Arthur Ming <937765518@qq.com>
2017-09-03 20:46:06 +0800

    add replace method

--- src/main/java/org/aming/gwms/utils/StringUtils.java
+++ src/main/java/org/aming/gwms/utils/StringUtils.java
@@ -17,4 +17,18 @@ public class StringUtils extends org.apache.commons.lang.StringUtils {
     public static String reflectionToString (Object obj){
         return ToStringBuilder.reflectionToString(obj);
     }
+    
+    public static String replace(String prefix,String suffix,String text,String...parameters){
+    	if(StringUtils.isBlank(text) || parameters == null || parameters.length < 1){
+    		return text;
+    	}
+    	String replace = null;
+		String paraValue = null;
+		for(int i = 0;i < parameters.length;i++){
+			paraValue = parameters[i] == null ? "" : parameters[i];
+			replace = prefix + i + suffix;
+			text = replace(text,replace,paraValue);
+		}
+    	return text;
+    }
 }
