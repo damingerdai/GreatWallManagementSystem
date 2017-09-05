82d84b3a5c5978ad592e9904b09d40755cb7e2f0
Arthur.Ming <arthur.ming@acxiom.com>
2017-09-05 17:51:57 +0800

    finish verify method

--- src/main/java/org/aming/gwms/dao/impl/LoginDaoImpl.java
+++ src/main/java/org/aming/gwms/dao/impl/LoginDaoImpl.java
@@ -1,6 +1,8 @@
 package org.aming.gwms.dao.impl;
 
 import org.aming.gwms.dao.LoginDao;
+import org.aming.gwms.jdbc.JdbcUtils;
+import org.aming.gwms.utils.EncodeUtils;
 
 /**
  * @author daming
@@ -9,9 +11,9 @@ import org.aming.gwms.dao.LoginDao;
 public class LoginDaoImpl implements LoginDao {
 	
 
-    private final static String SQL_VERIFY = "SELECT COUNT(SELECT COUNT(empid) FROM employee WHERE username = ? AND `password` = ? ";
+    private final static String SQL_VERIFY_USER = "SELECT COUNT(SELECT COUNT(empid) FROM employee WHERE username = ? AND `password` = ? ";
 
     public int verify(String name, String password) {
-        return 0;
+        return JdbcUtils.queryCount(SQL_VERIFY_USER,new Object[]{name, EncodeUtils.md5Hex(password)});
     }
 }
