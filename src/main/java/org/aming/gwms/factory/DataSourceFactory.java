fecc98a2dbad3032c0b95933b63cb03c8b4947b9
Arthur.Ming <arthur.ming@acxiom.com>
2017-09-05 15:20:24 +0800

    update newDataSource method

--- src/main/java/org/aming/gwms/factory/DataSourceFactory.java
+++ src/main/java/org/aming/gwms/factory/DataSourceFactory.java
@@ -7,29 +7,34 @@ import org.aming.gwms.utils.PropertyLoader;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
+import java.beans.PropertyVetoException;
+
 public class DataSourceFactory {
-	
-	private static DataSource dataSource;
-	
-	public static DataSource newDataSource(){
-		try{
-			ComboPooledDataSource dataSource = new ComboPooledDataSource();
-			PropertyLoader propertyLoader = PropertyLoader.getInstance("/jdbc.properties");
-			dataSource.setDriverClass(propertyLoader.getProperty("driver"));
-			return dataSource;
-		} catch(Exception e){
-			throw Exceptions.toRuntimeException(e);
-		}	
-	}
-	
-	public static DataSource getDataSource(){
-		if(dataSource == null){
-			synchronized (DataSourceFactory.class){
-				if(dataSource == null){
-					dataSource = newDataSource();
-				}
-			}
-		}
-		return dataSource;
-	}
+    private static DataSource dataSource;
+
+    public static DataSource getDataSource() {
+        try {
+            if (dataSource == null) {
+                synchronized (DataSourceFactory.class) {
+                    if (dataSource == null) {
+                        dataSource = newDataSource();
+                    }
+                }
+            }
+            return dataSource;
+        } catch (Exception ex) {
+            throw Exceptions.toRuntimeException(ex);
+        }
+
+    }
+
+    private static DataSource newDataSource() throws PropertyVetoException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        PropertyLoader propertyLoader = PropertyLoader.getInstance("/jdbc.properties");
+        dataSource.setDriverClass(propertyLoader.getProperty("driver"));
+        dataSource.setJdbcUrl(propertyLoader.getProperty("jdbcUrl"));
+        dataSource.setUser(propertyLoader.getProperty("user"));
+        dataSource.setPassword(propertyLoader.getProperty("password"));
+        return dataSource;
+    }
 }
