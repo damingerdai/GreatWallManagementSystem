6b09dee42a08a7917b1695e9bee3df8283643a5f
Arthur.Ming <arthur.ming@acxiom.com>
2017-09-05 16:05:45 +0800

    add a static method that throwable --> AmingException

--- src/main/java/org/aming/gwms/exceptions/Exceptions.java
+++ src/main/java/org/aming/gwms/exceptions/Exceptions.java
@@ -1,12 +1,24 @@
 package org.aming.gwms.exceptions;
 
+import java.sql.SQLException;
+
 public class Exceptions {
-	public static RuntimeException toRuntimeException(Throwable cause){
-		if(cause instanceof RuntimeException){
-			return (RuntimeException)cause;
-		}else{
-			return new RuntimeException(cause);
-		}
-	}
-	
+    public static RuntimeException toRuntimeException(Throwable cause) {
+        if (cause instanceof RuntimeException) {
+            return (RuntimeException) cause;
+        } else {
+            return new RuntimeException(cause);
+        }
+    }
+
+    public static AmingException toAmingException(Throwable cause){
+        if(cause instanceof AmingException){
+            return (AmingException)cause;
+        } else if(cause instanceof SQLException){
+            SQLException ex = (SQLException)cause;
+            return new AmingException(String.valueOf(ex.getErrorCode()),ex.getMessage(),ex.getCause());
+        } else{
+            return new AmingException(cause);
+        }
+    }
 }
