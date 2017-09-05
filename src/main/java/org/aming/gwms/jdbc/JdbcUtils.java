3f7fef6b48b654f6e207aec45b1819ee82e2b1e6
Arthur.Ming <arthur.ming@acxiom.com>
2017-09-05 14:03:35 +0800

    rename JdbcTemplate --> JdbcUitls

---
+++ src/main/java/org/aming/gwms/jdbc/JdbcUtils.java
@@ -0,0 +1,20 @@
+package org.aming.gwms.jdbc;
+
+
+/**
+ * jdbc模板
+ * @author daming
+ * @version 2017/7/4.
+ */
+public class JdbcUtils {
+
+    public static int excute(String sql){
+
+        try{
+
+        }catch (Exception ex){
+
+        }
+        return 0;
+    }
+}
